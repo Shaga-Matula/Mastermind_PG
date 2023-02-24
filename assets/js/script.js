a7ed2b5f7f54ace989b33faa461e2639498884c0
Paul-Gleeson <Shagamatula@gmail.com>
2023-02-24 16:55:46 +0000

    add timer for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM fully loaded and parsed');
-    // document.body.style.backgroundColor = 'blue';
+
+    var timerVariable = setInterval(countUpTimer, 1000);
+    var totalSeconds = 0;
+
+    function countUpTimer() {
+        ++totalSeconds;
+        var hour = Math.floor(totalSeconds / 3600);
+        var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+        var seconds = totalSeconds - (hour * 3600 + minute * 60);
+        document.getElementById("count_up_timer").innerHTML = hour + ":" + minute + ":" + seconds;
+    }
+
 });
