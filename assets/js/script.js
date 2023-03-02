9a86fec5a7535196496996db69e2253b376e9dfa
Paul-Gleeson <Shagamatula@gmail.com>
2023-03-02 15:03:56 +0000

    add event listen click to buttons and test

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
         var seconds = totalSeconds - (hour * 3600 + minute * 60);
         document.getElementById("count_up_timer").innerHTML = hour + ":" + minute + ":" + seconds;
     }
+    let new_game = document.getElementById("new-game");
+    new_game.addEventListener("click", function () {alert("You Clicked New Game");
+    });
 
+    let change_answer= document.getElementById("change-answer");
+    change_answer.addEventListener("click", function () {alert("You Clicked Change Answer");
+    });
+
+    let submit_answer= document.getElementById("submit-answer");
+    submit_answer.addEventListener("click", function () {alert("You Clicked Submit Answer");
+    });
 });
