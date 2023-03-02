f4fd7a800d0acaeb96af491c30ce7609f3d88428
Paul-Gleeson <Shagamatula@gmail.com>
2023-03-02 15:56:55 +0000

    add event listeners on all color buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,28 +11,50 @@ document.addEventListener('DOMContentLoaded', function () {
         var seconds = totalSeconds - (hour * 3600 + minute * 60);
         document.getElementById("count_up_timer").innerHTML = hour + ":" + minute + ":" + seconds;
     }
+
     let new_game = document.getElementById("new-game");
-    new_game.addEventListener("click", function () {alert("You Clicked New Game");
+    new_game.addEventListener("click", function () {
+        alert("You Clicked New Game");
     });
 
-    let change_answer= document.getElementById("change-answer");
-    change_answer.addEventListener("click", function () {alert("You Clicked Change Answer");
+    let change_answer = document.getElementById("change-answer");
+    change_answer.addEventListener("click", function () {
+        alert("You Clicked Change Answer");
     });
 
-    let submit_answer= document.getElementById("submit-answer");
-    submit_answer.addEventListener("click", function () {alert("You Clicked Submit Answer");
+    let submit_answer = document.getElementById("submit-answer");
+    submit_answer.addEventListener("click", function () {
+        alert("You Clicked Submit Answer");
     });
 
-    let c_red= document.getElementById("c-red");
-    c_red.addEventListener("click", function () {alert("You Clicked Red");
+    let c_red = document.getElementById("c-red");
+    c_red.addEventListener("click", function () {
+        alert("You Clicked Red");
     });
 
-    let c_blue= document.getElementById("c-blue");
-    c_blue.addEventListener("click", function () {alert("You Clicked blue");
+    let c_blue = document.getElementById("c-blue");
+    c_blue.addEventListener("click", function () {
+        alert("You Clicked Blue");
     });
 
-    let c_green= document.getElementById("c-green");
-    c_green.addEventListener("click", function () {alert("You Clicked green");
+    let c_green = document.getElementById("c-green");
+    c_green.addEventListener("click", function () {
+        alert("You Clicked Green");
+    });
+
+    let c_yellow = document.getElementById("c-yellow");
+    c_yellow.addEventListener("click", function () {
+        alert("You Clicked Yellow");
+    });
+
+    let c_black = document.getElementById("c-black");
+    c_black.addEventListener("click", function () {
+        alert("You Clicked Black");
+    });
+
+    let c_white = document.getElementById("c-white");
+    c_white.addEventListener("click", function () {
+        alert("You Clicked White");
     });
 
 });
